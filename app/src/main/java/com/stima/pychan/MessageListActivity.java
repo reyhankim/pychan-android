b6e5e9e56300748cf874a5bf1a81df65b8688c71
Reyhan Naufal Hakim <reyhan_kim@icloud.com>
2019-04-19 16:07:21 +0700

    Implemented intent to switch to MessageListActivity

--- app/src/main/java/com/stima/pychan/MessageListActivity.java
+++ app/src/main/java/com/stima/pychan/MessageListActivity.java
@@ -21,13 +21,6 @@ public class MessageListActivity extends AppCompatActivity {
     private MessageListAdapter mMessageAdapter;
     private ArrayList<Message> messageList;
 
-    private final String namaFilePertanyaan = "DataPertanyaan.txt";
-    private final String namaFileSynonym = "DataSynonym.txt";
-    private final String namaFileStopWords = "DataStopWords.txt";
-    private ArrayList<String> dataPertanyaan = getDataFromFile(namaFilePertanyaan);
-    private ArrayList<String> dataSynonym = getDataFromFile(namaFileSynonym);
-    private ArrayList<String> dataStopWords = getDataFromFile(namaFileStopWords);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,31 +29,5 @@ public class MessageListActivity extends AppCompatActivity {
         mMessageRecycler = findViewById(R.id.reyclerview_message_list);
         mMessageAdapter = new MessageListAdapter(this, messageList);
         mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
-    }
-
-    //Prosedur pembacaan file dan memindahkan ke memori agar data dari file tersimpan
-    public static ArrayList<String> getDataFromFile(String namaFile){
-
-        ArrayList<String> data = new ArrayList<String>();
-
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(namaFile));
-            String temp = null;
-            temp = br.readLine();
-
-            while(temp!=null){
-                data.add(temp);
-                temp = br.readLine();
-            }
-
-        }catch(FileNotFoundException e){
-            System.out.println("Tidak dapat membuka file " + namaFile);
-            System.exit(1);
-        }catch(IOException e){
-            System.out.println("Gagal membaca file " + namaFile);
-            System.exit(1);
-        }
-
-        return data;
     }
 }
